e83aba182370cd526d4ee781f26bb5bb50d1615d
RyanQuey <rlquey2@Gmail.com>
2017-02-21 18:10:49 -0800

    reorganize commands into separate methods and 2 files; successfully begin uploading large file process

--- backup_script_template.rb
+++ backup_script_template.rb
@@ -1,32 +1,28 @@
 require_relative './large_files'
 include LargeFiles
 
-#Either put in the exact bucket namehere, if you want to specify the bucket, or else put in "prompt me" if you want to be prompted each time you run the script. Make sure to use the correct capitalization and spacingfor the bucket name.
-@chosen_bucket_name = ""
+#Either put in the exact bucket name here, if you want to specify the bucket, or else leave in "prompt me" if you want to be prompted each time you run the script. Make sure to use the correct capitalization and spacing for the bucket name.
+
+@chosen_bucket_name = "prompt me"
+@filename_of_upload = "test_for_upload.txt"
+size_of_file = "large"
+
+
+
+#Either put in the exact file/folder name here, if you want to specify the file/folder, or else leave in "prompt me" if you want to be prompted each time you run the script. Make sure to use the correct capitalization and spacing for the file/folder name.
+# Note that this can be a file or a folder
+
 
 # Eventually move all of this code in turn it into helper methods in place in other documents, to keep this more clean.
-if @chosen_bucket_name == "prompt me"
-  puts "Available buckets:"
-  buckets_list.each do |b|
-    puts b["bucketName"]
-  end
-  puts "Which Bucket do you want to upload to?" 
-  @chosen_bucket_name = gets.chomp
-end
-
-buckets_list_response.each_with_index do |b, i|
-  if b["bucketName"] == @chosen_bucket_name
-    @chosen_bucket_index = i
-  end
-end
-
-# Note that this can be a file or a folder
-@file_to_upload = 
-
 
 # Place "large" or "regular" depending on whether or not you are uploading large files or regular files. If you're not sure, see the backblaze documentation on their website.
-size_of_files = ""
 
-if size_of_files == "large"
-  upload_large_files
-elsif size_of_files == "regular"
+list_and_choose_bucket
+specify_file_upload_info
+
+
+
+if size_of_file == "large"
+  start_large_file
+elsif size_of_file == "regular"
+end
--- large_files.rb
+++ large_files.rb
@@ -13,9 +13,9 @@
 require 'backblaze'
 require 'pry'
 
-#module LargeFiles
+module LargeFiles
   include Backblaze::B2
-  # might need to include backblaze
+  # might not? need to include backblaze
 #  def upload_large_files
   
     ## Authorize Account (b2_authorize_account) ##
@@ -24,28 +24,88 @@ require 'pry'
     
     ##
     ## b2_list_buckets ##
+  def list_and_choose_bucket
     
-    # borrowing from backblaze b2's own documentation, found on their website
-    #Backblaze::B2::Base
-    
+    #TODO: Eventually want to do something like this:
+    #buckets_list_response = Bucket.buckets
+    #This would be using the backblaze gem method more directly. However, I wasn't able to figure out how it work exactly offhand, so will have to return to this later if I get the chance. In the meantime:
     buckets_list_response = HTTParty.post("#{Backblaze::B2::Base.base_uri}/b2_list_buckets", 
       body: {accountId: ENV['ACCOUNT_ID']}.to_json,
       headers: Backblaze::B2::Base.headers
-    ) 
-    number_of_buckets = buckets_list_response.length
+    ) #returns an array
 
     # Basically, to pick a different bucket, replace 0 with the appropriate number. Can do this either using gets.chomp or manually configuring backup_script_template.
-    chosen_bucket = buckets_list_response["buckets"][0]
+    if @chosen_bucket_name == "prompt me"
+      puts "Available buckets:"
+      buckets_list_response["buckets"].each_with_index do |b, i|
+        print i+1 
+        puts ") " + b["bucketName"]
+      end
+      puts "Which Bucket do you want to upload to? (insert number and press enter)" 
+      @chosen_bucket_number = gets.chomp.to_i-1
+
+      if @chosen_bucket_number > buckets_list_response["buckets"].length || @chosen_bucket_number < 0 
+        #TODO Eventually assign a real error message
+        puts "Invalid number: There is no such bucket for that number"
+        return
+      end
+      @chosen_bucket_hash = buckets_list_response["buckets"][@chosen_bucket_number]
+    else # I.e., if the @chosen_bucket_name is an actual bucket name:
+      buckets_list_response["buckets"].each do |b|
+        if @chosen_bucket_name == b["bucketName"]
+          @chosen_bucket_hash = b #uckets_list_response["buckets"][@chosen_bucket_index]
+          
+        end
+      end
+    end
     
-    bucket = Bucket.new(bucket_name: chosen_bucket["bucketName"], bucket_id: chosen_bucket["bucketId"], bucket_type: chosen_bucket["bucketType"], account_id: chosen_bucket["accountId"])
-    puts bucket.file_names
-    ##
-    ##b2_start_large_file
-  
-  
+    #This assigns the chosen bucket to a ruby object. I might decide to ultimately not use the backblaze gem as a dependency at all though, and just work directly with the API.
+    bucket_object = Bucket.new(bucket_name: @chosen_bucket_hash["bucketName"], bucket_id: @chosen_bucket_hash["bucketId"], bucket_type: @chosen_bucket_hash["bucketType"], account_id: @chosen_bucket_hash["accountId"])
+    puts bucket_object.inspect
+  end #end of list_and_choose_bucket method
+
+  def specify_file_upload_info
+    if @filename_of_upload == "prompt me"
+      puts "Available files:"
+      #Need to find a way to browse through the directories using Ruby
+      filenames_list.each do |f|
+        puts f["fileName"]
+      end
+      puts "Which file do you want to upload?" 
+      @filename_of_upload = gets.chomp
+    end
+
+    #Might remove this function, or else decided automatically depending on the type of backup.
+    puts "Manually decide content type?[y/n] (If answer is not y, it will be assumed that the answer is no)"
+    user_response = gets.chomp
     
-#  end 
-  #
-#end
+    if user_response == "y" # or "Y"
+      @content_type = gets.chomp
+    else 
+      @content_type = "b2/x-auto"
+    end
+  end
+
+
+  def start_large_file
+    response = HTTParty.post("#{Backblaze::B2::Base.base_uri}/b2_start_large_file", 
+      body: {
+        bucketId: @chosen_bucket_hash["bucketId"],
+        fileName: "#{@filename_of_upload}",
+        contentType: "#{@content_type}"
+        #could also eventually incorporate fileInfo parameter here
+      }.to_json,
+      headers: Backblaze::B2::Base.headers
+    ) 
+   puts @chosen_bucket_hash["bucketId"]
+   puts response
+  end
+  def list_filenames
+
+    bucket.file_names.each do |f|
+     puts  f.file_name #(or something like this)
+    end
+  end
+end #(of the module)
 
 
